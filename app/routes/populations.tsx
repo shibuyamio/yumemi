c41d01653d5a8fab82d6864cb2eb55dfc52d924c
shibuya <mio.shibuya@trictrac.co.jp>
2022-04-24 20:28:51 +0900

    フォーム部分作成

--- app/routes/populations.tsx
+++ app/routes/populations.tsx
@@ -1,6 +1,6 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData, useSubmit } from "@remix-run/react";
 import { getPrefectureItems } from "~/models/population.server";
 
 export type Prefecture = {
@@ -18,8 +18,20 @@ export const loader: LoaderFunction = async () => {
   return json<LoaderData>({ prefectureItems });
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  console.log(formData);
+  return null;
+};
+
 export default function Populations() {
   const data = useLoaderData() as LoaderData;
+  const submit = useSubmit();
+
+  function handleChange(event: React.ChangeEvent<HTMLFormElement>) {
+    submit(event.currentTarget, { replace: true });
+  }
 
   return (
     <div className="flex h-full min-h-screen flex-col">
@@ -35,56 +47,41 @@ export default function Populations() {
             都道府県
           </h2>
           <div className="h-64 overflow-auto">
-            <fieldset className="grid grid-cols-3 gap-4 border-t border-b border-gray-200 sm:grid-cols-4">
-              {data.prefectureItems.map((prefecture) => {
-                const inputId = `pref_${prefecture.prefCode}`;
-                return (
-                  <div
-                    key={prefecture.prefCode}
-                    className="relative flex items-start py-2"
-                  >
-                    <div className="mr-3 flex h-5 items-center">
-                      <input
-                        id={inputId}
-                        aria-describedby="candidates-description"
-                        name="candidates"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                    </div>
-                    <div className="min-w-0 flex-1 text-sm">
-                      <label
-                        htmlFor={inputId}
-                        className="font-medium text-gray-700"
-                      >
-                        {prefecture.prefName}
-                      </label>
-                      {/*                     <p id="candidates-description" className="text-gray-500">
+            <Form method="post" onChange={handleChange}>
+              <fieldset className="grid grid-cols-3 gap-4 border-t border-b border-gray-200 sm:grid-cols-4">
+                {data.prefectureItems.map((prefecture) => {
+                  const inputId = `pref_${prefecture.prefCode}`;
+                  return (
+                    <div
+                      key={prefecture.prefCode}
+                      className="relative flex items-start py-2"
+                    >
+                      <div className="mr-3 flex h-5 items-center">
+                        <input
+                          id={inputId}
+                          aria-describedby="prefectures-description"
+                          name={`${prefecture.prefCode}`}
+                          type="checkbox"
+                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                      </div>
+                      <div className="min-w-0 flex-1 text-sm">
+                        <label
+                          htmlFor={inputId}
+                          className="font-medium text-gray-700"
+                        >
+                          {prefecture.prefName}
+                        </label>
+                        {/*                     <p id="candidates-description" className="text-gray-500">
                       Get notified when a candidate applies for a job.
                     </p> */}
+                      </div>
                     </div>
-                  </div>
-                  /*                 <div
-                  key={prefecture.prefCode}
-                  className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-                >
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src="https://via.placeholder.com/150/0000FF/808080"
-                      alt=""
-                    />
-                  </div>
-                  <a href="#" className="focus:outline-none">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">
-                      {prefecture.prefName}
-                    </p>
-                  </a>
-                </div> */
-                );
-              })}
-            </fieldset>
+                  );
+                })}
+              </fieldset>
+              <button type="submit">submit</button>
+            </Form>
           </div>
         </div>
         <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
